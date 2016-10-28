eee11ee6acc8fa347a4c1c6f22db42d5a2f315e9
Collo <delanhype@gmail.com>
2016-10-28 14:33:32 +0300

    Pushed code having function that generates prime numbers.Though it has a bug.It return 1 which is wrong

---
+++ prime-number-function.py
@@ -0,0 +1,19 @@
+
+def primeNumbers(num): #function declaration
+    
+    x = [1] 
+    begin = 3 
+    while True:
+        for i in range(3, int(begin**0.5) + 1):  
+            if begin % i == 0 or begin % 2 == 0: 
+                break
+        else:
+            x.append(begin) #append the number thats not divisible by 
+        begin += 1
+        if begin == num:
+            break
+            
+    return x #return list of prime numbers
+
+
+print primeNumbers(80)
